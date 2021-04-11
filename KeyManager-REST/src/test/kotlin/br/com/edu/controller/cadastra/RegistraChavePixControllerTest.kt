fc9318a73baeb1beb8c532ec97658a346dd17719
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-11 17:11:51 -0300

    testes

---
+++ KeyManager-REST/src/test/kotlin/br/com/edu/controller/cadastra/RegistraChavePixControllerTest.kt
@@ -0,0 +1,15 @@
+package br.com.edu.controller.cadastra
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+internal class RegistraChavePixControllerTest{
+    @Test
+    fun `criar uma nova chave pix`(){ // usar `xxx`
+        //cenario
+        // acao
+        // validacao
+    }
+}
